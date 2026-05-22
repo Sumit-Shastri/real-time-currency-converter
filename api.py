c56e06d1143d03a98444613d0230bfb9f4c85cd6
Sumit-Shastri <shastrisumit25@gmail.com>
2026-05-22 12:36:32 +0530

    App menu initiallisation

---
+++ api.py
@@ -0,0 +1,50 @@
+"""
+-----------------------------------------------------------------------
+ imports
+-----------------------------------------------------------------------
+"""
+
+
+
+"""
+-----------------------------------------------------------------------
+ Methods
+-----------------------------------------------------------------------
+"""
+
+
+"""
+-----------------------------------------------------------------------
+    Method Name    : currency_converter()
+    arguments      : convert_from      -->     currency to exchange
+                     amount            -->     amount to exchange
+                     convert_to        -->     exchanging currency
+    output         : converted Amount
+    description    : This function takes currency type as input and 
+                     returns the currency converted by latest exchange
+                     rate.
+-----------------------------------------------------------------------
+"""
+
+def currency_converter(convert_from, amount, convert_to):
+    print("Feature available soon !.")
+
+
+"""
+-----------------------------------------------------------------------
+    Method Name    : view_history()
+    arguments      : None
+    output         : list of searches
+    description    : This function function returns the whole hostory
+                     of user searches on currency exchange.
+-----------------------------------------------------------------------
+"""
+
+def view_history():
+    print("Feature available soon.")
+
+"""
+-----------------------------------------------------------------------
+ END
+-----------------------------------------------------------------------
+"""
---
+++ main.py
@@ -0,0 +1,77 @@
+"""
+-----------------------------------------------------------------------
+ Imports
+-----------------------------------------------------------------------
+"""
+
+import sys
+from api import currency_converter
+from api import view_history
+"""
+-----------------------------------------------------------------------
+ Main
+-----------------------------------------------------------------------
+"""
+
+menu = """
+1. Convert Currency
+2. View history
+3. Favorites
+4. Convert All
+5. Exit\n
+"""
+
+currencies = """
+1. INR ( Indian Rupee )
+2. USD ( United State Dollar )
+3. EUR ( EURO ) 
+4. JPY ( Japanese YEN )
+5. GBP ( British Pound )
+6. CNY ( Chinese Yuan )
+7. CHF ( Swiss Franc )
+8. AUD ( Australian Dollar )
+9. CAD ( Canadian Dollar )
+10. NZD ( New Zealand Dollar )\n
+"""
+
+print("\n---   Welcome to Real-Time-Currency-COnverter ---\n")
+
+while(True):
+    choose_menu = int(input(menu))
+
+    match (choose_menu):
+        case 1:
+            print("--- Currency Converter ---")
+            print(currencies)
+            convert_from = int(input("Convert From : "))
+            amount = int(input("Amount : "))
+            convert_to = int(input("Convert To : "))
+
+            currency_converter(convert_from, amount, convert_to)
+
+        case 2:
+            print("--- View History ---")
+
+            view_history()
+
+        case 3:
+            print("--- Favorites ---")
+
+            #favorites()
+
+        case 4:
+            print("--- Convert All ---")
+
+            convert_from = input("Enter currency code (eg. INR) : ")
+
+            #convert_all(convert_from)
+
+        case 5:
+            print("Exiting ...")
+            sys.exit()
+
+"""
+-----------------------------------------------------------------------
+ END
+-----------------------------------------------------------------------
+"""
